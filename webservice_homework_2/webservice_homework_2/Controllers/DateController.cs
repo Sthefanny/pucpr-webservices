c020528997335c31b1d8402c26fd54aa4710e0f3
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2021-06-07 20:48:28 -0300

    Exercise 01 done

---
+++ webservice_homework_2/webservice_homework_2/Controllers/DateController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webservice_homework_2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DateController : Controller
+    {
+        [HttpGet("GetDays")]
+        public int GetDays(DateTime initialDate, DateTime finalDate)
+        {
+            return (int)(finalDate - initialDate).TotalDays;
+        }
+
+        [HttpGet("GetWeeks")]
+        public int GetWeeks(DateTime initialDate, DateTime finalDate)
+        {
+            return (int)(finalDate - initialDate).TotalDays / 7;
+        }
+
+        [HttpGet("GetMonths")]
+        public int GetMonths(DateTime initialDate, DateTime finalDate)
+        {
+            return (int)(finalDate.Subtract(initialDate).Days / (365.25 / 12));
+        }
+    }
+}
---
+++ webservice_homework_2/webservice_homework_2/Models/DateModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace webservice_homework_2.Models
+{
+    public class DateModel
+    {
+        public int days;
+        public int weeks;
+        public int months;
+    }
+}
