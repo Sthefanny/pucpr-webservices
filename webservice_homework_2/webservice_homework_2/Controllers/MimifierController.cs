54ea7b912b72bbe053d99fff11ba6dc4070ab9a7
Sthefanny Gonzaga <sthefannygonzaga@gmail.com>
2021-06-07 21:36:07 -0300

    Exercises 02 and 03

---
+++ webservice_homework_2/webservice_homework_2/Controllers/MimifierController.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webservice_homework_2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MimifierController : ControllerBase
+    {
+        [HttpGet("Mimify")]
+        public string Mimify(string text)
+        {
+            var regex = "[aeiouAEIOUáéíóúÁÉÍÓÚâêîôûÂÊÎÔÛ]";
+           return Regex.Replace(text, regex, "i");
+        }
+    }
+}
---
+++ webservice_homework_2/webservice_homework_2/Controllers/NumbersController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace webservice_homework_2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NumbersController : Controller
+    {
+        [HttpGet("GetAscendingOrder")]
+        public string GetAscendingOrder(string numbersAsString)
+        {
+            string[] numbers = numbersAsString.Split(';');
+
+            var result = numbers.OrderBy(i => i);
+
+            return string.Join(";", result);
+        }
+
+        [HttpGet("GetDescendingOrder")]
+        public string GetDescendingOrder(string numbersAsString)
+        {
+            string[] numbers = numbersAsString.Split(';');
+
+            var result = numbers.OrderByDescending(i => i);
+
+            return string.Join(";", result);
+        }
+
+        [HttpGet("GetEven")]
+        public string GetEven(string numbersAsString)
+        {
+            string[] numbers = numbersAsString.Split(';');
+
+            var OddEven = new List<int>();
+
+            foreach (var i in numbers)
+                OddEven.Add(int.Parse(i.ToString()));
+
+            var even = new List<int>();
+
+            foreach (int i in OddEven)
+            {
+                if (i % 2 == 0)
+                    even.Add(i);
+            }
+
+            return string.Join(";", even);
+        }
+    }
+}
